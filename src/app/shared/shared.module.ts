a940ae998c45fdf56a3337977784d513b5c17b4c
Thibaud Pellissier <thibaudpellissier@pltmail.fr>
2021-12-25 16:18:05 +0100

    Big update

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SearchCardComponent } from './search-card/search-card.component';
+
+@NgModule({
+  declarations: [SearchCardComponent],
+  imports: [CommonModule],
+})
+export class SharedModule {}
